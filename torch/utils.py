b27d4af6bc5edb6e9316b51db15ed5511a40074c
Emre Sahin <emre.sahin95@web.de>
2026-01-30 14:45:22 +0100

    modified experimental

--- torch/utils.py
+++ torch/utils.py
@@ -1,10 +1,19 @@
 import torch 
 
+def move(f):
+    def moved(model, *args, **kwargs):
+        # Get device
+        device = next(model.parameters()).get_device()
+
+        # run at cpu and move back
+        model = model.to("cpu")
+        res = f(model, *args, **kwargs)
+        model = model.to(device)
+
+        return res
+    return moved
 
 def train(model, dataLoader, criterion, optimizer, epochs=1):
-    '''
-    Trains model for specified number of epochs
-    '''
     # Get device
     device = next(model.parameters()).get_device()
 
@@ -32,34 +41,21 @@ def train(model, dataLoader, criterion, optimizer, epochs=1):
         print(f"Avg loss: {loss:>8f} \n")
 
 
+@move
 def test(model, dataLoader, criterion):
-    '''
-    Evaluate mean test loss over given DataLoader
-    '''
-    # Get device
-    device = next(model.parameters()).get_device()
-
     # Evaluate loss
     model.eval()
     with torch.no_grad():
         loss = 0.0
         for X, y in dataLoader:
-            X, y = X.to(device), y.to(device)
-
             pred = model(X)
             loss += criterion(pred, y).item()
     loss /= len(dataLoader)
     
     return loss
     
-
+@move
 def confusion(model, dataLoader):
-    '''
-    Compute confusion matrix and prediction accuracy for multi-class-classification
-    '''
-    # Get device
-    device = next(model.parameters()).get_device()
-    
     # number of classes
     model.eval()
     k = 0
@@ -73,8 +69,6 @@ def confusion(model, dataLoader):
     C = torch.zeros((k,k))
     with torch.no_grad():
         for X, y in dataLoader:
-            X, y = X.to(device), y.to(device)
-
             c = model.predict(X)
             for i in range(len(X)):
                 C[c[i], y[i]] += 1
